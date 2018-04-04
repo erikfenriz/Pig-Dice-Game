03947f7f6a7450c07650ec8d379bd0b487db9d68
Ernestovich <erikfenriz@gmail.com>
2018-04-04 14:49:38 +0300

    Add files via upload

---
+++ scripts/scroll.js
@@ -0,0 +1,25 @@
+let scrollY = 0;
+let distance = 50;
+let speed = 24;
+let show = document.querySelectorAll("section[class='hide']");
+
+function autoScrollTo(el) {
+    let currentY = window.pageYOffset;
+    let targetY = document.getElementById(el).offsetTop;
+    let bodyHeight = document.body.offsetHeight;
+    let yPos = currentY + window.innerHeight;
+    let animator = setTimeout('autoScrollTo(\'' + el + '\')', speed);
+
+    if (yPos > bodyHeight) {
+        clearTimeout(animator);
+    } else {
+        if (currentY < targetY - distance) {
+            scrollY = currentY + distance + 100;
+            window.scroll(0, scrollY);
+        } else {
+            clearTimeout(animator);
+            show[0].classList.remove('hide');
+        }
+    }
+
+}
